e5a000427e4ad3fbe54415b341146b281656b24d
wuttichaihung <wuttichaihung@gmail.com>
2023-03-04 02:48:37 +0700

    add predict tensorflow model

--- flask_app.py
+++ flask_app.py
@@ -1,23 +1,30 @@
 from flask import Flask, request, jsonify
-from werkzeug.utils import secure_filename
+from tensorflow.keras.applications.inception_v3 import InceptionV3, decode_predictions
+import numpy as np
+import cv2
+
 app = Flask(__name__)
 
+# Load the model
+# model = tf.keras.models.load_model('my_model.h5')
+model = InceptionV3()
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return jsonify(resutl=0, msg="Not found image", status="fail")
-        f = request.files['file']
-        file_path = secure_filename(f.filename)
-        f.save(file_path)
 
-        # process data
-        result = 1
-        return jsonify(resutl=result, msg="", status="success")
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the uploaded file
+    file = request.files['file']
 
-    if request.method == 'GET':
-        return "Hello World"
+    # Read the image and preprocess it
+    img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), -1)
+    img = cv2.resize(img, (299, 299))
+    img = img.astype('float32') / 255.0
+
+    # Make a prediction using the model
+    prediction = model.predict(img.reshape(1, 299, 299, 3))
+    decoded_pred = decode_predictions(prediction)[0]
+    # Return the prediction as a JSON object
+    return jsonify({'prediction': [(t[1], t[2].item()) for t in decoded_pred]})
 
 
 if __name__ == '__main__':
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import requests
-url = "http://localhost:5000/"
 
-uploaded_file = st.file_uploader("Choose a file")
+# Create a file upload widget
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+# If an image is uploaded
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    file_path = uploaded_file.name
-    with open(file_path, 'wb') as f:
-        f.write(bytes_data)
-    res = requests.post(url, files={'file': open(file_path, 'rb')})
-    st.write(res.json())
+
+    # Display the image
+    st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
+
+    # Make a POST request with the uploaded file
+    url = "http://localhost:5000/predict"  # Replace with your own endpoint
+    files = {"file": uploaded_file.getvalue()}
+    response = requests.post(url, files=files)
+
+    # Display the response
+    st.write("Response:", response.text)
